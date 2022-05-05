788a796948b44b87a866e4d58b9abf0123244475
Matko Barbic <matko.barbic@fer.hr>
2022-05-05 17:46:28 +0200

    Can recive img

--- lil/src/components/Guess.js
+++ lil/src/components/Guess.js
@@ -11,6 +11,7 @@ const Guess = () => {
             <h2 className="title-small">Coordinates:</h2>
             <h2 className="title-small">{(location.state.x * (19.45 - 13.5) + 13.5).toFixed(7)} N</h2>
             <h2 className="title-small">{(location.state.y * (46.55 - 42.4) + 42.4).toFixed(7)} E</h2>
+            <img src={location.state.image}></img>
             <button className="button" onClick={handleClick}>New location</button>
         </div>
     )
