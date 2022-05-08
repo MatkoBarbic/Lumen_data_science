338011143b6f5b1cbb5b64d3d1a97cc0d4a09a4f
Matko Barbic <matko.barbic@fer.hr>
2022-05-08 18:24:33 +0200

    Minor change

--- backend/myapp/views.py
+++ backend/myapp/views.py
@@ -9,8 +9,4 @@ import main
 # Create your views here.
 @csrf_exempt
 def index(request):
-    # print(request.FILES)
-    # my_file = request.FILES['0.jpg']
-    # filename = my_file.read()
-    # print("aksfbasdklflasdbffffffffffffffffffffffffadn", request.FILES['0.jpg'].temporary_file_path())
     return JsonResponse(main.predict_coords(request))
